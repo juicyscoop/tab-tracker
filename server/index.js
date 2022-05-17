ef8ddd1240c5832a23c1090f76dee4492b972470
Oskar Marelja <oskarmarelja@gmail.com>
2022-05-17 15:56:47 +0200

    Setup of Express.js server

---
+++ server/index.js
@@ -0,0 +1,22 @@
+console.log('app started ')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(morgan('combined'))
+
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/status', (req, res) => {
+    res.send({
+        message:'hello wrold'
+    })
+})
+
+app.listen(process.env.PORT || 8081)
+
+
